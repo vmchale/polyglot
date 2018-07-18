63ac9468d61e6364acb571093ee00ed94cd08cf8
Vanessa McHale <vanessa.mchale@iohk.io>
2018-07-17 22:48:34 -0500

    install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -3,6 +3,7 @@
 set -e
 set pipefail
 
+# FIXME - this should work for other/obscure architectures
 getTarget() {
     if [ "$(uname)" = "Darwin" ]
     then
@@ -16,12 +17,15 @@ main() {
 
     latest="$(curl -s https://github.com/vmchale/polyglot/releases/latest/ | cut -d'"' -f2 | rev | cut -d'/' -f1 | rev)"
     binname=$(getTarget)
+
     mkdir -p "$HOME/.local/bin"
     mkdir -p "$HOME/.local/share/man/man1/"
     mkdir -p "$HOME/.compleat"
+
     dest=$HOME/.local/bin/poly
     man_dest=$HOME/.local/share/man/man1/poly.1
     compleat_dest=$HOME/.compleat/poly.usage
+
     if command -v wget > /dev/null ; then
         wget https://github.com/vmchale/polyglot/releases/download/"$latest"/"$binname" -O "$dest"
         wget https://github.com/vmchale/polyglot/releases/download/"$latest"/poly.1 -O "$man_dest"
